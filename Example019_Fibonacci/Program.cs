151e26ba928ab89f61c55b3dc1bd3b87dd218d52
Mikhail <Gusev_74@mail.ru>
2022-08-21 22:52:22 +0300

    add Example019 Fibonacci

---
+++ Example019_Fibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿// f(1) = 1;
+// f(2) = 1;
+// f(3) = (3 - 1) + (3 - 2) = 3
+// f(4) = (4 - 1) + (4 - 2) = 5
+// f(5) = (5 - 1) + (5 - 2) = 7
+// f(n) = (n - 1) + (n - 2)
+int Fibonacci(int n)
+{
+    if (n == 1 || n ==2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n + 1);
+}
+
+for(int i = 0; i < 5; i++)
+{
+Console.WriteLine(Fibonacci(i));
+}
+
