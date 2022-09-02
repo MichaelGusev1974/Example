7391064823fcf010fa8212e12b98faa083bccc8d
Mikhail <Gusev_74@mail.ru>
2022-09-02 18:33:15 +0300

    add Example022For, Example022Recursion

---
+++ Example022For/Program.cs
@@ -0,0 +1,9 @@
+﻿// Факториал числа (Факториалом числа называют произведением чисел от 1 до n) итеративный способ.
+int FactorialFor(int n)
+{
+    int result = 1; // Factorial 0! = 1;
+    for(int i = 1; i <= n; i++)
+    result *= i;
+    return result;
+}
+Console.WriteLine(FactorialFor(10));
---
+++ Example022Recursion/Program.cs
@@ -0,0 +1,7 @@
+﻿// Факториал числа (Факториалом числа называют произведением чисел от 1 до n) рекурсивный способ.
+int FactorialRec(int n)
+{
+    if(n == 1) return 1;               // Factorial 0! = 1;
+    else return n * FactorialRec(n-1);
+}
+Console.WriteLine(FactorialRec(10));
