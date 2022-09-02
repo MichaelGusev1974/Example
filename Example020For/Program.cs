950c9778e0ad9952555665b87d99e454f77d912b
Mikhail <Gusev_74@mail.ru>
2022-09-02 17:49:45 +0300

    add Example020For, Example020Recursion

---
+++ Example020For/Program.cs
@@ -0,0 +1,23 @@
+﻿// Собрать строку с числами от а до b, где a <= b итеративный способ;
+// string NumbersFor(int a, int b)
+// {
+//     string result = string.Empty;
+//     for(int i = a; i <= b; i++)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+// Console.WriteLine(NumbersFor(1,10));
+
+// Собрать строку с числами от а до b, где a >= b итеративный способ;
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for(int i = a; i >= b; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+Console.WriteLine(NumbersFor(10,1));
---
+++ Example020Recursion/Program.cs
@@ -0,0 +1,19 @@
+﻿// Собрать строку с числами от а до b, где a <= b рекурсивный способ;
+// string NumbersRec(int a, int b)
+// {
+//     if(a <= b) 
+//     return $"{a} " + NumbersRec(a + 1,b);
+//     else
+//     return string.Empty;
+// }
+// Console.WriteLine(NumbersRec(1,10));
+
+//Собрать строку с числами от а до b, где a >= b рекурсивный способ;
+ string NumbersRec(int a, int b)
+ {
+     if(a <= b) 
+     return  NumbersRec(a + 1,b) + $"{a} ";
+     else
+     return string.Empty;
+ }
+ Console.WriteLine(NumbersRec(1,10));
