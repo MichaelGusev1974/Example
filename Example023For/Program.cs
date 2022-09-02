c93cf3fb2f324236ab28d407fefc2a99b07be005
Mikhail <Gusev_74@mail.ru>
2022-09-02 18:50:23 +0300

    add Example023For, Example023Recursion

---
+++ Example023For/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вычисление числа а в натуральной степени n итеративный способ.
+int PowerFor(int a, int n)
+{
+    int result = 1; 
+    for(int i = 1; i <= n; i++)
+    result *= a;
+    return result;
+}
+Console.WriteLine(PowerFor(3, 2));
---
+++ Example023Recursion/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вычисление числа а в натуральной степени n рекурсивный способ.
+int PowerFor(int a, int n)
+{
+    if(n == 0) return 1;
+    else return a * PowerFor( a, n -  1);
+}
+Console.WriteLine(PowerFor(3, 3));
