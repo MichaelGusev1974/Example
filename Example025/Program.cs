63fb05a080c66d4c819083468e9681a4c0c669f5
Mikhail <Gusev_74@mail.ru>
2022-09-15 16:18:37 +0300

    add Example024, Example025

---
+++ Example025/Program.cs
@@ -0,0 +1,4 @@
+﻿//Написание в редакторе .NET кода для преобразования заданного значения температуры по шкале Фаренгейта в градусы по Цельсию.
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32) * (5m / 9);
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
