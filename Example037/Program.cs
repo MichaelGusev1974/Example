4a94669e4439e9a6eaec2bd7d72704ae545e7243
Mikhail <Gusev_74@mail.ru>
2022-09-19 00:47:11 +0300

    add Example 037, 038

---
+++ Example037/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача на вынесение переменных из блоков кщда для определения видимости переменных.
+/*int[] numbers = { 4, 8, 15, 16, 23, 42 };
+foreach (int number in numbers)
+{
+    int total;
+    total += number;
+    if (number == 42)
+    {
+       bool found = true;
+    }
+}
+if (found) 
+{
+    Console.WriteLine("Set contains 42");
+}
+Console.WriteLine($"Total: {total}");*/
+// Переделать код:
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)found = true;
+}
+if (found) Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
